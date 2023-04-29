6622a380797bed0ed552083a24dfdb463f242858
k-zyw <jianwuliyuan448@gmail.com>
2023-04-29 17:42:51 +0900

    商品の出品情報画面の編集

--- app/models/item.rb
+++ app/models/item.rb
@@ -11,11 +11,16 @@ class Item < ApplicationRecord
   belongs_to       :user
   has_one_attached :image
 
-  extend ActiveHash::Associations::ActiveRecordExtensions #←アクティブハッシュ継承(アソシエーションを組む)
+  extend ActiveHash::Associations::ActiveRecordExtensions #←アクティブハッシュ継承(アソシエーションを組む)14~25行目まで
   belongs_to :category
   belongs_to :product_condition
   belongs_to :cost
   belongs_to :shipping_day
   belongs_to :prefecture
-   
+
+  validates :category_id,           numericality:{ other_than: 1 , message: "can't be blank" } #←ジャンルの選択が「---」の時は保存できないようにする
+  validates :product_condition_id,  numericality:{ other_than: 1 , message: "can't be blank" }
+  validates :cost_id,               numericality:{ other_than: 1 , message: "can't be blank" }
+  validates :shipping_day_id,       numericality:{ other_than: 1 , message: "can't be blank" }
+  validates :prefecture_id,         numericality:{ other_than: 1 , message: "can't be blank" }
 end
