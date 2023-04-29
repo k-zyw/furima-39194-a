585533b4f4c3c4302aa6d260c1dfd11fd2d2b63a
k-zyw <jianwuliyuan448@gmail.com>
2023-04-29 14:13:51 +0900

    app/models内のuser.rb及びitem.rbのバリテーションとアソシエーション編集

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,12 @@
 class Item < ApplicationRecord
+  validates  :product_name,           presence: true
+  validates  :price,                  presence: true
+  validates  :description,            presence: true
+  validates  :category_id,            presence: true
+  validates  :product_condition_id,   presence: true
+  validates  :cost_id,                presence: true
+  validates  :shipping_day_id ,       presence: true      
+  validates  :prefecture_id,          presence: true
+
+  belongs_to :user
 end
