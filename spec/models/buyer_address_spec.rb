424b07341ace03be49859285320fceaccaa01a2f
k-zyw <jianwuliyuan448@gmail.com>
2023-05-05 14:07:48 +0900

    商品購入(配送先)単体テストコード異常系

--- spec/models/buyer_address_spec.rb
+++ spec/models/buyer_address_spec.rb
@@ -17,5 +17,68 @@ RSpec.describe BuyerAddress, type: :model do
         expect(@buyer_address).to be_valid        
       end
     end
+
+    context '商品購入(配送先情報)が入力できない場合' do
+      it 'userが紐付いていないと保存できない' do
+        @buyer_address.user_id = nil
+        @buyer_address.valid?
+        expect(@buyer_address.errors.full_messages).to include("User can't be blank")
+      end
+      it 'itemが紐付いていないと保存できない' do
+        @buyer_address.item_id = nil
+        @buyer_address.valid?
+        expect(@buyer_address.errors.full_messages).to include("Item can't be blank")
+      end
+      it 'post_cord(郵便番号)が空なら保存できない' do
+        @buyer_address.post_cord = ''
+        @buyer_address.valid?        
+        expect(@buyer_address.errors.full_messages).to include("Post cord can't be blank")
+      end
+      it 'post_cord(郵便番号)が「3桁ハイフン4桁」の半角文字列でないと保存できない' do
+        @buyer_address.post_cord = '1234567'
+        @buyer_address.valid?        
+        expect(@buyer_address.errors.full_messages).to include("Post cord is invalid")
+      end
+      it 'prefecture_id(発送先の地域)が「---」なら保存できない' do
+        @buyer_address.prefecture_id = 1
+        @buyer_address.valid?        
+        expect(@buyer_address.errors.full_messages).to include("Prefecture must be other than 1")
+      end
+      it 'city(市区町村)が空なら保存できない' do
+        @buyer_address.city = ''
+        @buyer_address.valid?        
+        expect(@buyer_address.errors.full_messages).to include("City can't be blank")
+      end
+      it 'address(番地)が空なら保存できない' do
+        @buyer_address.address = ''
+        @buyer_address.valid?        
+        expect(@buyer_address.errors.full_messages).to include("Address can't be blank")
+      end
+      it 'phone_number(電話番号)が空なら保存できない' do
+        @buyer_address.phone_number = ''
+        @buyer_address.valid?        
+        expect(@buyer_address.errors.full_messages).to include("Phone number can't be blank")
+      end
+      it 'phone_number(電話番号)が全角数字だと保存できない' do
+        @buyer_address.phone_number = '０９０１２３４５６７８'
+        @buyer_address.valid?        
+        expect(@buyer_address.errors.full_messages).to include("Phone number is invalid")
+      end
+      it 'phone_number(電話番号)が「ハイフン」有りだと保存できない' do
+        @buyer_address.phone_number = '090-12345678'
+        @buyer_address.valid?        
+        expect(@buyer_address.errors.full_messages).to include("Phone number is invalid")
+      end
+      it 'phone_number(電話番号)が9桁以下だと保存できない' do
+        @buyer_address.phone_number = '090123456'
+        @buyer_address.valid?             
+        expect(@buyer_address.errors.full_messages).to include("Phone number is invalid")
+      end
+      it 'phone_number(電話番号)が12桁以上だと保存できない' do
+        @buyer_address.phone_number = '090123456789'
+        @buyer_address.valid?             
+        expect(@buyer_address.errors.full_messages).to include("Phone number is invalid")
+      end
+    end
   end
 end
