132c1e55c207085d63bf8439e225072eccad820a
k-zyw <jianwuliyuan448@gmail.com>
2023-05-14 15:55:35 +0900

    commit

--- config/deploy.rb
+++ config/deploy.rb
@@ -2,7 +2,9 @@
 lock "~> 3.17.2"
 
 set :application, "furima_39194_a"
-set :repo_url, "git@github.k-zyw/furima-39194-a.git"
+# どのリポジトリからアプリをpullするかを指定する
+set :repo_url, "git@github.com:k-zyw/furima-39194-a.git"
+
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
 set :rbenv_type, :user
 set :rbenv_ruby, '2.6.5'
